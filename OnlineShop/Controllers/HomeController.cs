8a06c5ac5a47923e6fc07489bc923bbd008a0cd8
Loc <locnbhe153182@fpt.edu.vn>
2025-02-22 14:54:00 +0700

    setup env

---
+++ OnlineShop/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShop.Interfaces;
+using OnlineShop.Models;
+using System.Diagnostics;
+
+namespace OnlineShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IPhotoService _photoService;
+        public HomeController(ILogger<HomeController> logger, IPhotoService photoService)
+        {
+            _logger = logger;
+            _photoService = photoService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile imageFile)
+        {
+            if (imageFile != null && imageFile.Length > 0)
+            {
+                var result = await _photoService.AddPhotoAsync(imageFile);
+                ViewBag.Message = "Ảnh đã tải lên thành công!";
+                ViewBag.FileName = result.Url.ToString();
+                return View("Index");
+            }
+
+            ViewBag.Message = "Vui lòng chọn một file!";
+            return View("Index");
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OnlineShop/Program.cs
@@ -0,0 +1,55 @@
+﻿using CloudinaryDotNet;
+using dotenv.net;
+using OnlineShop.Helper;
+using OnlineShop.Interfaces;
+using OnlineShop.Services;
+
+namespace OnlineShop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews().AddRazorRuntimeCompilation();
+            builder.Services.AddScoped<IPhotoService,PhotoService>();
+            DotEnv.Load(options: new DotEnvOptions(probeForEnv: true));
+            var cloudinarySettings = new CloudinarySettings
+            {
+                CloudName = Environment.GetEnvironmentVariable("CLOUDINARY_CLOUDNAME"),
+                ApiKey = Environment.GetEnvironmentVariable("CLOUDINARY_APIKEY"),
+                ApiSecret = Environment.GetEnvironmentVariable("CLOUDINARY_APISECRET")
+            };
+            builder.Services.Configure<CloudinarySettings>(options =>
+            {
+                options.CloudName = cloudinarySettings.CloudName;
+                options.ApiKey = cloudinarySettings.ApiKey;
+                options.ApiSecret = cloudinarySettings.ApiSecret;
+            });
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
